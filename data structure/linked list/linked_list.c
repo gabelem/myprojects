cefcca9b25a5c6589fd3d51ab653d6fc346b433e
belem <gabelem.moraes@gmail.com>
2026-04-09 10:36:14 -0400

    another linked list test

---
+++ data structure/linked list/linked_list.c
@@ -0,0 +1,74 @@
+typedef struct node
+{
+    int val;
+    struct node *next;
+} node_t;
+
+
+//print all items of a list
+void print_list(node_t *head)
+{
+    node_t *current = head;
+
+    while(current != NULL)
+    {
+        printf("%d\n", current->val);
+        current = current->next;
+    }
+}
+
+//"while" go to the last item of the list (NULL) and after that add the value 3, and add one more node on the list with NULL.
+void push_end(node_t *head, int val)
+{
+    node_t *current = head;
+
+    while(current->next != NULL)
+    {
+        current = current->next;
+    }
+
+    current->next = (node_t *) malloc(sizeof(node_t));
+    current->next->val = val;
+    current->next->next; NULL;
+}
+
+/*1 - struct *new_node has been created
+  2 - new_node got allocated dynamically
+  3 - we set the value for this new_node via argument
+  4 - new_node is now = *head??
+  5 - and the head got the value of new_node??
+  
+  before?:
+  (new_node)->(head)
+  
+  after?:
+  (head)->(new_node)*/
+void push_begin(node_t **head, int val)
+{
+    node_t *new_node;
+    new_node = (node_t *) malloc(sizeof(node_t));
+
+    new_node->val = val;
+    new_node->next = *head;
+    *head = new_node;
+}
+
+int main()
+{
+    node_t *head = NULL; //empty head
+    
+    head = (node_t *) malloc(sizeof(node_t)); // memory alloc
+
+    if(head == NULL) //check if head is null
+    {
+        return 1;
+    }
+
+    head-> = 1; //head now have the int value = 1
+    head->next = NULL //at the 1st moment the next node is NULL.
+
+    head->next = (node_t *) malloc(sizeof(node_t)); //memory alloc of the next node after "head".
+
+    head->next->val = 2;
+    head->next->next = NULL; //3rd/last node NULL.
+}
