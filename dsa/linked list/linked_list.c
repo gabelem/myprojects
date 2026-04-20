f09419a6fdf804d3f3a9fe054a74d9219f1ba2fb
belem <gabelem.moraes@gmail.com>
2026-04-20 07:52:19 -0400

    dsa

---
+++ dsa/linked list/linked_list.c
@@ -0,0 +1,153 @@
+typedef struct node
+{
+    int val;
+    struct node *next;
+} node_t;
+
+
+//print all items of a list
+void print_list(node_t *head)
+{
+    node_t *current = head;
+
+    while(current != NULL)
+    {
+        printf("%d\n", current->val);
+        current = current->next;
+    }
+}
+
+//"while" go to the last item of the list (NULL) and after that add the value 3, and add one more node on the list with NULL.
+void push_end(node_t *head, int val)
+{
+    node_t *current = head;
+
+    while(current->next != NULL)
+    {
+        current = current->next;
+    }
+
+    current->next = (node_t *) malloc(sizeof(node_t));
+    current->next->val = val;
+    current->next->next; NULL;
+}
+
+/*1 - struct *new_node has been created
+  2 - new_node got allocated dynamically
+  3 - we set the value for this new_node via argument
+  4 - new_node is now = *head??
+  5 - and the head got the value of new_node??
+  
+  before?:
+  (new_node)->(head)
+  
+  after?:
+  (head)->(new_node)*/
+void push_begin(node_t **head, int val)
+{
+    node_t *new_node;
+    new_node = (node_t *) malloc(sizeof(node_t));
+
+    new_node->val = val;
+    new_node->next = *head;
+    *head = new_node;
+}
+
+
+//
+int pop(node_t **head)
+{
+    int retval = -1;
+    node_t *next_node= NULL;
+
+    if(*head == NULL)
+    {
+        return -1;
+    }
+
+    next_node = (*head)->next;
+    retval = (*head)->val;
+    free(*head);
+    *head = next_node;
+
+    return retval;
+}
+
+//
+int remove_last(node_t *head)
+{
+    int retval = 0;
+
+    if(head->next == NULL)
+    {
+        retval = head->val;
+        free(head);
+        return retval;
+    }
+    
+    node_t *current = head;
+    while(current->next->next != NULL)
+    {
+        current = current->next;
+    }
+    retval = current->next->val;
+    free(current->next);
+    current->next = NULL;
+    return retval;
+}
+
+//
+int removeByIndex(node_t ** head, int n)
+{
+    int i = 0;
+    int retval = -1;
+    node_t *current = *head;
+    node_t *temp_node = NULL;
+
+    if(n == 0)
+    {
+        return pop(head);
+    }
+
+    for(i = 0; i < n-1; i++)
+    {
+        if(current->next == NULL)
+        {
+            return -1;
+        }
+        current = current->next;
+
+        if(current->next == NULL)
+        {
+            return -1;
+        }
+    }
+
+    temp_node = current->next;
+    retval = temp_node->val;
+    current->next = temp_node->next;
+
+    free(temp_node);
+
+    return retval;
+}
+
+int main()
+{
+    node_t *head = NULL; //empty head
+    
+    head = (node_t *) malloc(sizeof(node_t)); // memory alloc
+
+    if(head == NULL) //check if head is null
+    {
+        return 1;
+    }
+
+    head-> = 1; //head now have the int value = 1
+    head->next = NULL //at the 1st moment the next node is NULL.
+
+    head->next = (node_t *) malloc(sizeof(node_t)); //memory alloc of the next node after "head".
+
+    head->next->val = 2;
+    head->next->next = NULL; //3rd/last node NULL.
+}
---
+++ dsa/linked list/linked_list_without_functions.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+struct node *head;
+
+int main()
+{
+    struct node *first = (struct node*) malloc(sizeof(struct node));
+    struct node *second = (struct node*) malloc(sizeof(struct node));
+
+    head = first;
+
+    first->data = 1;
+    first->next = second;
+
+    second->data = 2;
+    second->next = NULL;
+
+
+    if(head == NULL)
+    {
+        printf("a lista encadeada não foi iniciada\n");
+        return 0;
+    }
+    else
+    {
+        printf("%d %d\n", head->data, second->data);
+        
+        free(second);
+        //second = NULL;
+
+        free(first);
+        //first->next = NULL;
+
+        //free(head);
+        first = NULL;
+        second = NULL;
+        head = NULL;
+
+        printf("A lista foi desalocada\n");
+    }
+    
+    return 0;
+}
